e2a62178fd6a8d44feef1f6bcc8f401f7d2217bc
Omar AbdEl-Hady <omar@omarabdelhady.me>
2018-07-02 03:31:12 +0200

    Add: Chat base app

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDKpqwzCL7mrUOO37aWxdl71Qo4ZlwFgcg",
+    authDomain: "chat-app-7eb6d.firebaseapp.com",
+    databaseURL: "https://chat-app-7eb6d.firebaseio.com",
+    projectId: "chat-app-7eb6d",
+    storageBucket: "",
+    messagingSenderId: "802035207735"
+  }
+};
